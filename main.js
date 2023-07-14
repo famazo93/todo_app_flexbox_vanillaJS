b2e6579a8a5ed40bb21f21ead425b79f2b963cc6
famazo <mfarkas@ehrenfeld.hu>
2023-07-14 16:43:46 +0200

    option to remove task added

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 
 const todoField = document.getElementById('container-field');
+let taskCounter = 0;
 
 
 const todoElement = (text, date, prio) => {
-    return `<div class="todo added">
+    return `<div class="todo added" id="task-${taskCounter}">
                 <div class="todo-text">${text}</div>
                 <div class="todo-date">${date}</div>
                 <div class="todo-prio">${prio}</div>
-                <button id="remove">X</button>
+                <button id="remove-${taskCounter}">X</button>
             </div>`
 };
 
 const submitTask = (event) => {
+    taskCounter++;
     event.preventDefault();
     const todoText = document.getElementById('new-task').value;
     let todoDate = document.getElementById('task-deadline').value;
@@ -25,7 +27,20 @@ const submitTask = (event) => {
         window.alert('Please add a description to your todo!')
     }
 
+    let currentTask = document.getElementById(`remove-${taskCounter}`);
+    currentTask.addEventListener('click', removeTask);
+};
+
+const removeTask = (event) => {
+    const id = event.target.parentElement.id;
+    event.preventDefault();
+    const toRemove = document.getElementById(id);
+    toRemove.remove();
 }
 
 const submit = document.getElementById('submit');
 submit.addEventListener('click', submitTask);
+
+// this is only here for the test task
+const remove = document.getElementById('remove');
+remove.addEventListener('click', removeTask);
