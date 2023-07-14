1ddac8b5ce9ae89d7415dc61617e511069ebf4c2
famazo <mfarkas@ehrenfeld.hu>
2023-07-14 15:22:07 +0200

    CSS for added tasks fixed

--- main.js
+++ main.js
@@ -3,19 +3,27 @@ const todoField = document.getElementById('container-field');
 
 
 const todoElement = (text, date, prio) => {
-    return `<div class="todo">
-                <div>${text}</div>
-                <div>${date}</div>
-                <div>${prio}</div>
+    return `<div class="todo added">
+                <div class="todo-text">${text}</div>
+                <div class="todo-date">${date}</div>
+                <div class="todo-prio">${prio}</div>
+                <button id="remove">X</button>
             </div>`
 };
 
 const submitTask = (event) => {
     event.preventDefault();
     const todoText = document.getElementById('new-task').value;
-    const todoDate = document.getElementById('task-deadline').value;
-    const todoPrio = document.getElementById('priority').value;
-    todoField.insertAdjacentHTML("beforeend", todoElement(todoText, todoDate, todoPrio))
+    let todoDate = document.getElementById('task-deadline').value;
+    let todoPrio = document.getElementById('priority').value;
+
+    if (todoText) {
+        todoDate ? todoDate = todoDate : todoDate = 'No Deadline';
+        todoPrio ? todoPrio = todoPrio : todoPrio = 'No Prio';
+        todoField.insertAdjacentHTML("beforeend", todoElement(todoText, todoDate, todoPrio))
+    } else {
+        window.alert('Please add a description to your todo!')
+    }
 
 }
 
