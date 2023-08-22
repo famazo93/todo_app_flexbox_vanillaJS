c04dd43ed943c499c2f9cdcc6ad88836fa96fc14
famazo <mfarkas@ehrenfeld.hu>
2023-08-22 20:01:31 +0200

    users DB converted to JSON, adding users working

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const store = new session.MemoryStore();
-const users = require('./database/users.js');
+const fs = require('fs');
 
 const PORT = 3000;
 
@@ -48,6 +48,10 @@ app.delete('/todos/:id', (req, res, next) => {
     res.send(todo);
 })
 
+app.get('/login/newUser', (req, res, next) => {
+    res.render('newUser')
+})
+
 app.get('/login', (req, res, next) => {
     if (!req.session.authenticated){
         res.render('login');
@@ -65,25 +69,60 @@ app.get('/todos', (req, res, next) => {
     }
 })
 
+app.post('/login/newUser', (req, res, next) => {
+    const { username, password } = req.body;
+    fs.readFile('./database/users.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            const { users } = JSON.parse(data);
+            const lastId = users[users.length - 1].id;
+            users.push({
+                id: Number(lastId) + 1,
+                username,
+                password
+            })
+            fs.writeFile('./database/users.json', JSON.stringify({users}), (err) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.render('login');
+                }
+            })
+        }
+    })
+})
+
 app.post('/login', (req, res, next) => {
     const { username, password } = req.body;
-    users.findByUsername(username, (err, user) => {
-        if (!user) {
-            res.status(403).send('No user found')
+    fs.readFile('./database/users.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
         } else {
-            if (user.password === password) {
-                req.session.authenticated = true;
-                req.session.user = {
-                    username,
-                    password,
-                };
-                res.redirect('todos');
+            const { users } = JSON.parse(data);
+            let selectedUser = null;
+            users.forEach(user => {
+                if (user.username === username) {
+                    selectedUser = user;
+                }
+            });
+            if (!selectedUser) {
+                res.status(404).send('No user found');
             } else {
-                res.status(403).send('Wrong password');
+                if (selectedUser.password === password) {
+                    req.session.authenticated = true;
+                    req.session.user = {
+                        username,
+                        password,
+                    };
+                    res.redirect('todos');
+                } else {
+                    res.status(403).send('Wrong password');
+                }
             }
         }
         console.log(req.session);
-    });
+    })
 })
 
 app.listen(PORT, () => {
